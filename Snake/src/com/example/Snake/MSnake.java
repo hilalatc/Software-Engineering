ff2c9bd66dd9e421b213f0790a981b211fbcdce2
Hilal Atıcı <hilalatc@gmail.com>
2013-12-04 16:48:46 +0200

    Software Engineering

---
+++ Snake/src/com/example/Snake/MSnake.java
@@ -0,0 +1,13 @@
+package com.example.Snake;
+
+import android.app.Activity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hilal
+ * Date: 04.12.2013
+ * Time: 16:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class MSnake extends Activity {
+}
---
+++ Snake/src/com/example/Snake/SnakeView.java
@@ -0,0 +1,11 @@
+package com.example.Snake;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hilal
+ * Date: 04.12.2013
+ * Time: 16:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class SnakeView {
+}
