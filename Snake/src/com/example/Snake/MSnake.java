0e1e51dfddd22b63e996f8706ee778bf475107a0
Hilal Atıcı <hilalatc@gmail.com>
2013-12-04 17:09:43 +0200

    layout eklendi.

--- Snake/src/com/example/Snake/MSnake.java
+++ Snake/src/com/example/Snake/MSnake.java
@@ -1,13 +1,33 @@
 package com.example.Snake;
 
 import android.app.Activity;
+import android.widget.RadioGroup;
 
 /**
  * Created with IntelliJ IDEA.
  * User: Hilal
- * Date: 04.12.2013
+ * Date: 10.11.2013
  * Time: 16:32
  * To change this template use File | Settings | File Templates.
  */
 public class MSnake extends Activity {
+    private SnakeView mSnakeView;
+
+    private static String ICICLE_KEY = "snake-view";
+
+    static final int DIALOG_ABOUT_ID = 0;
+    static final int DIALOG_RECORDS_ID = 1;
+    static final int DIALOG_WALLS_ID = 2;
+    static final int DIALOG_SIZE_ID = 3;
+    static final int DIALOG_SPEED_ID = 4;
+    static final int DIALOG_NEWS_ID = 5;
+    static final int DIALOG_EXIT_ID = 6;
+    static final int DIALOG_INPUT_ID = 7;
+    static final int DIALOG_SETTINGS_ID = 8;
+    static final int DIALOG_WARMSETTINGS_ID = 9;
+
+    private RadioGroup smsrg1;
+    private RadioGroup smsrg2;
+    private RadioGroup smsrg3;
+    private RadioGroup smsrg4;
 }
--- Snake/src/com/example/Snake/SnakeView.java
+++ Snake/src/com/example/Snake/SnakeView.java
@@ -3,7 +3,7 @@ package com.example.Snake;
 /**
  * Created with IntelliJ IDEA.
  * User: Hilal
- * Date: 04.12.2013
+ * Date: 10.11.2013
  * Time: 16:33
  * To change this template use File | Settings | File Templates.
  */
