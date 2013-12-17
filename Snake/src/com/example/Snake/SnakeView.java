45a6df383392dbf9ce73c42cd7c9671a39df939e
Hilal Atıcı <hilalatc@gmail.com>
2013-12-17 23:48:15 +0200

    SnakeView

--- Snake/src/com/example/Snake/SnakeView.java
+++ Snake/src/com/example/Snake/SnakeView.java
@@ -929,6 +929,63 @@ public class SnakeView extends TileView {
         }
     }
 
+    void drawSnakeBad() {
+        int index = 0;
+        for (Coordinate c : mSnakeTrail) {
+            if (index == 0) {
+                setTile(HEADBAD_TILE, c.x, c.y);
+            } else {
+                setTile(BODY_TILE, c.x, c.y);
+            }
+            index++;
+        }
+    }
 
+    /**
+     *
+     */
+    private void updateScore() {
+        CharSequence str = "";
+        Resources res = getContext().getResources();
+
+        str = " " + res.getString(R.string.score) + " " + mScore;
+        mScoreText.setText(str);
+
+        str = res.getString(R.string.record) + " " + mRecords[indRecord] + " ";
+        mRecordText.setText(str);
+
+//        + "        " + numcollision
+//        + "   " + dWidth + "," + dHeight;
+//        + "   " + "Delay" + mMoveDelay;
+    }
+
+    /**
+     * Simple class containing two integer values and a comparison function.
+     * There's probably something I should use instead, but this was quick and
+     * easy to build.
+     *
+     */
+    private class Coordinate {
+        public int x;
+        public int y;
+
+        public Coordinate(int newX, int newY) {
+            x = newX;
+            y = newY;
+        }
+
+        public boolean equals(Coordinate other) {
+            if (x == other.x && y == other.y) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "Coordinate: [" + x + "," + y + "]";
+        }
+    }
 
 }
+
